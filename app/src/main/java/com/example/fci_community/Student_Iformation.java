af69f29a96078e216c33ca2878305c6e3f13fa9e
NahlaFadl <nahla.fadl2002@gmail.com>
2020-04-30 00:38:25 +0200

    radiobutton

--- app/src/main/java/com/example/fci_community/Student_Iformation.java
+++ app/src/main/java/com/example/fci_community/Student_Iformation.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +19,7 @@ public class Student_Iformation extends AppCompatActivity {
      private   EditText St_Name,St_mail,St_pass,St_id,St_phot;
      public static   StudentModuel stm;
      public static String stName;
+     RadioGroup groups;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference();
     @Override
@@ -26,7 +28,57 @@ public class Student_Iformation extends AppCompatActivity {
         setContentView(R.layout.activity_student__iformation);
         compinitial();
 
+        groups.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                stName=St_Name.getText().toString();
+                String stid=St_id.getText().toString();
+                String stMail=St_mail.getText().toString();
+                String stPas=St_pass.getText().toString();
+
+                if (checkedId==R.id.group_one)
+                {
+                    Toast.makeText(Student_Iformation.this,"group one",Toast.LENGTH_SHORT).show();
+
+                    stm =new StudentModuel(stName,stMail,stPas,stid);
+                    myRef.child("Groups").child("GroupOne").
+                            child("Students").child(stid).setValue(stm);
+
+                }
+
+                else if (checkedId==R.id.group_two)
+                {
+                    Toast.makeText(Student_Iformation.this,"group two",Toast.LENGTH_SHORT).show();
+
+
+                    stm =new StudentModuel(stName,stMail,stPas,stid);
+                    myRef.child("Groups").child("GroupTwo").
+                            child("StudentsTwo").child(stid).setValue(stm);
+                }
+
+                else if (checkedId==R.id.group_three)
+                {
+                    Toast.makeText(Student_Iformation.this,"group three",Toast.LENGTH_SHORT).show();
+
+                    stm=new StudentModuel(stName,stMail,stPas,stid);
+                    myRef.child("Groups").child("GroupThree").
+                            child("StudentsThree").child(stid).setValue(stm);
+                }
+
+                else if (checkedId==R.id.group_four)
+                {
+                    Toast.makeText(Student_Iformation.this,"group four",Toast.LENGTH_SHORT).show();
+
+                    stm=new StudentModuel(stName,stMail,stPas,stid);
+                    myRef.child("Groups").child("GroupFour")
+                            .child("StudentsFour").child(stid).setValue(stm);
+                }
+            }
+        });
+
     }
+
 
 private void compinitial(){
 
@@ -35,9 +87,13 @@ private void compinitial(){
     St_pass=(EditText)findViewById(R.id.StudentPass);
     St_id=(EditText)findViewById(R.id.StudentID);
     St_id=(EditText)findViewById(R.id.StudentID);
+    groups=(RadioGroup)findViewById(R.id.groups);
+
+
 
 }
     public void StudentData(View view) {
+
          stName=St_Name.getText().toString();
         String stid=St_id.getText().toString();
         String stMail=St_mail.getText().toString();
@@ -47,10 +103,12 @@ private void compinitial(){
 
             Toast.makeText(this, "Enter All Data", Toast.LENGTH_SHORT).show();
         } else {
-     stm =new StudentModuel(stName,stMail,stPas,stid);
+    /* stm =new StudentModuel(stName,stMail,stPas,stid);
         myRef.child("Groups").child("GroupOne").
-        child("Students").child(stid).setValue(stm);
+        child("Students").child(stid).setValue(stm);*/
         Intent intent=new Intent(Student_Iformation.this, St_mainactivity.class);
         startActivity(intent);}
     }
+
+
 }
