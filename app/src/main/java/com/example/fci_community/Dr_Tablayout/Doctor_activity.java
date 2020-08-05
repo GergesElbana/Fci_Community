c98fcfb06997789ef4b99b4d815756bd6cb85641
unknown <GergesElbana>
2020-08-05 17:00:01 +0200

    Splash

--- app/src/main/java/com/example/fci_community/Dr_Tablayout/Doctor_activity.java
+++ app/src/main/java/com/example/fci_community/Dr_Tablayout/Doctor_activity.java
@@ -21,16 +21,16 @@ public class Doctor_activity extends AppCompatActivity {
     }
     private void setViewPager() {
         Tab_adapter adapter = new Tab_adapter(getSupportFragmentManager());
-        adapter.addFragment(new dr_HomeFragment());
+      adapter.addFragment(new dr_HomeFragment());
         adapter.addFragment(new dr_Lecture());
-        adapter.addFragment(new dr_profile());
+         adapter.addFragment(new dr_profile());
         dr_vp=(ViewPager) findViewById(R.id.viewpager);
         dr_vp.setAdapter(adapter);
         dr_tp=(TabLayout)findViewById(R.id.tablayout);
         dr_tp.setupWithViewPager(dr_vp);
         dr_tp.getTabAt(0).setIcon(R.drawable.ic_home);
         dr_tp.getTabAt(1).setIcon(R.drawable.ic_lecter);
-        dr_tp.getTabAt(2).setIcon(R.drawable.ic_profile);
+         dr_tp.getTabAt(2).setIcon(R.drawable.ic_profile);
 
     }
 }
